b83707165115c7004e7676c5146cd150c7bb9e59
Kazim Reza <hl305@hotmail.com>
2021-02-20 05:23:28 +0000

    Redid the assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,27 +3,17 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName= "";
+let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "";
-let correctAnswer = "";
-let candidateAnswer = "";
-let questions = "";
-let correctAnswers = "";
-let candidateAnswers = "";
-let question2 = "";
-let candidateAnswer2 = "";
-let correctAnswer2 = "";
-let question3 = "";
-let candidateAnswer3 = "";
-let correctAnswer3 = "";
-let question4 = "";
-let candidateAnswer4 = "";
-let correctAnswer4 = "";
-let question5 = "";
-let candidateAnswer5 = "";
-let correctAnswer5 = "";
-
+let question;
+let correctAnswer;
+let candidateAnswer;
+let questions = ["Who was the first American woman in space? ","True or false: 5000 meters = 5 kilometers. "  ,"(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "True","40","Trajectory","3"];
+let candidateAnswers =[];
+let len = questions.length;
+let lowerAnswer = [];
+let lowerCandidateAnswer = [];
 
 
 function askForName() {
@@ -33,67 +23,48 @@ console.log(`Candidate Name: ${candidateName}\n`);
 }
 
 
-
 function askQuestion() {
-  question = 'Who was the first American woman in space? ';
-  correctAnswer = 'Sally Ride';
-  question2 = 'True or false: 5000 meters = 5 kilometers. ';
-  correctAnswer2 = 'True';
-  question3 = '(5 + 3)/2 * 10 = ? ';
-  correctAnswer3 = '40';
-  question4 = "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ";
-  correctAnswer4 = 'Trajectory';
-  question5 = 'What is the minimum crew size for the ISS? ';
-  correctAnswer5 = '3';
-
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question("\n1) " + question + "\nYour Answer: ");
-  console.log("Correct Answer: " + correctAnswer);
-  candidateAnswer2 = input.question("\n2) " + question2 + "\nYour Answer: ");
-  console.log("Correct Answer: " + correctAnswer2);
-  candidateAnswer3 = input.question("\n3) " + question3 + "\nYour Answer: ");
-  console.log("Correct Answer: " + correctAnswer3);
-  candidateAnswer4 = input.question("\n4) " + question4 + "\nYour Answer: ");
-  console.log("Correct Answer: " + correctAnswer4);
-  candidateAnswer5 = input.question("\n5) " + question5 + "\nYour Answer: ");
-  console.log("Correct Answer: " + correctAnswer5);
+ let l;
+  for (l = 0; l < len; l++) {
+    if (l > len) {
+      break;
+    }
+   answer = input.question(l+1+") " + questions[l] + "\nYour Answer: ");
+  console.log(`Correct Answer: ${correctAnswers[l]}\n`);
+  candidateAnswers.push(answer);
+}
+//console.log(candidateAnswer);
+lowerAnswer  = correctAnswers.map(correctAnswers => correctAnswers.toLowerCase());
+lowerCandidateAnswer = candidateAnswers.map(candidateAnswers => candidateAnswers.toLowerCase());
+//console.log(lowerAnswer);
+//console.log(lowerCandidateAnswer);
+
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  candidateAnswer1 = candidateAnswer.toLowerCase();
-  correctAnswer1 = correctAnswer.toLowerCase();
-  candidateAnswer21 = candidateAnswer2.toLowerCase();
-  correctAnswer21 = correctAnswer2.toLowerCase();
-  candidateAnswer41 = candidateAnswer4.toLowerCase();
-  correctAnswer41 = correctAnswer4.toLowerCase();
- 
-  let right = 0;
+
+
   let grade;
-  if (correctAnswer1 === candidateAnswer1) {
+  let right = 0;
+  let l;
+  for (l = 0; l < len; l++) {
+    if (l > len) {
+      break;
+    }
+ 
+      if (lowerAnswer[l] === lowerCandidateAnswer[l]) {
     right = right + 1;
   }
-   if (correctAnswer21 === candidateAnswer21) {
-    right = right + 1;
   } 
-  if (correctAnswer3 === candidateAnswer3) {
-    right = right + 1;
-  } 
-  if (correctAnswer41 === candidateAnswer41) {
-    right = right + 1;
-  } 
-  if (correctAnswer5 === candidateAnswer5) {
-    right = right + 1;
-  }
-  score = (right/5)*100;
+  grade = (right/5)*100;
   if (grade >= 80) {
     console.log(`\n>>> Overall Grad: ${grade}% (${right} out of 5 correct) <<<\n>>> Status: PASSED <<<`)
   } else {
-    console.log(`\n>>> Overall Grad: ${grade}% (${right} out of 5 correct) <<<\n>>> Status: FAILED <<<`);
+    console.log(`\n>>> Overall Grade: ${grade}% (${right} out of 5 correct) <<<\n>>> Status: FAILED <<<`);
   }
-
- 
   
 
   return grade;
